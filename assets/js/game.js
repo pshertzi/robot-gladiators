10c79c5548c8750c9cf68d38750fc36e672f8f30
Paul Shertzinger <pshertzi@gmail.com>
2021-12-23 14:00:25 -0500

    console

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,8 @@
-window.alert("Coco is a hoe!")
+var playerName = window.prompt("What is your robot's name?");
+console.log(playerName);
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+//fight();
